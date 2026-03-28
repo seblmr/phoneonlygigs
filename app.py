06e79c82c02db6e62aeaa438eab267fde07c06c5
seblmr <smouria@gmx.fr>
2026-03-28 11:17:41 +0100

    Add weekly gigs counter (social proof)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import sqlite3
-from datetime import datetime, datetimedelta
+from datetime import datetime, timedelta
 import stripe
 import os
 import json
